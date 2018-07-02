a47268272a321a9e890ef7acb5fc3bcd63005036
Wessel Schuurman <w.j.schuurman@student.tudelft.nl>
2018-07-02 16:31:59 +0200

    hoi test

--- index.php
+++ index.php
@@ -8,8 +8,21 @@
 	<body>
 		<h1>Welcome!</h1>
 		<?php
-			$content = the_content();
-			echo $content;
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post(); 
+					?>
+						<?php
+							$title = the_title();
+							$content = the_content();
+						?>
+
+						<?php the_title( '<h3>', '</h3>' ); ?>
+						<b><?php $content; ?></b>
+
+					<?php
+				} // end while
+			} // end if
 		?>
 		
 	</body>	
